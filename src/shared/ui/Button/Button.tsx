721ff2c587ee08fef2e335acd5db351cdf7af06d
CLoudXVII <sharkov.vs@outlook.com>
2025-07-20 19:08:36 +0300

    feat: Сделана кнопка c вариантами

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,27 @@
+import clsx from 'clsx';
+import React from 'react';
+
+import styles from './Button.module.css';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary' | 'tertiary';
+  children?: React.ReactNode;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  variant = 'primary',
+  children,
+  ...props
+}: ButtonProps) => {
+  const className = clsx(styles.button, {
+    [styles['button-primary']]: variant === 'primary',
+    [styles['button-secondary']]: variant === 'secondary',
+    [styles['button-tertiary']]: variant === 'tertiary',
+  });
+
+  return (
+    <button className={className} {...props}>
+      {children}
+    </button>
+  );
+};
