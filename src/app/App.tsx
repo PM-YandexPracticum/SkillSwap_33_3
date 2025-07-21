530c9e925a1fcfee51225e3933ce32756b397fba
Alexandra Vasileva <alexandravslv00@gmail.com>
2025-07-21 13:14:59 +0500

    feat: Добавлен footer

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,19 @@
-function App() {
-  return <div>123</div>;
-}
+import { BrowserRouter } from 'react-router-dom';
+import { Footer } from '../shared/ui/Footer';
+
+const AppContent = () => {
+  return (
+    <div>
+      <p>123</p>
+      <Footer />
+    </div>
+  );
+};
+
+const App = () => (
+  <BrowserRouter>
+    <AppContent />
+  </BrowserRouter>
+);
 
 export default App;
