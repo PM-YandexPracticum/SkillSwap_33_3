7cae12f2abbe23728b6efbbec1629759a66847a3
Alexandra Vasileva <alexandravslv00@gmail.com>
2025-07-22 15:54:35 +0500

    fix: добавление типа, который принимает просто строку

--- src/shared/ui/CardSkill/CardSkill.tsx
+++ src/shared/ui/CardSkill/CardSkill.tsx
@@ -3,21 +3,26 @@ import clsx from 'clsx';
 import styles from './CardSkill.module.css';
 import { getCategoryColor } from './categories';
 
-type CardSkillType = {
-  category: string;
-  skillName?: string;
-  subcategory?: string;
-  id?: number;
-};
+type CardSkillType =
+  | {
+      category: string;
+      skillName?: string;
+      subcategory?: string;
+      id?: number;
+    }
+  | string;
 
 interface CardSkillProps {
   skill: CardSkillType;
 }
 
 export const CardSkill: React.FC<CardSkillProps> = ({ skill }) => {
-  const label = skill.skillName ?? skill.subcategory ?? 'Неизвестный навык';
+  const label =
+    typeof skill === 'string' ? skill : (skill.skillName ?? skill.subcategory);
 
-  const categoryColor = getCategoryColor(skill.category);
+  const categoryColor = getCategoryColor(
+    typeof skill === 'string' ? 'Unknown' : skill.category
+  );
 
   return (
     <span className={clsx(styles.tag, styles[categoryColor])}>{label}</span>
