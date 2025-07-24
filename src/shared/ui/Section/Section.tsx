46cdc8f0cdf48011b9b1d1a18e6a7399abff700c
CLoudXVII <sharkov.vs@outlook.com>
2025-07-24 21:36:04 +0300

    feat: Создан компонент секции с карточками

---
+++ src/shared/ui/Section/Section.tsx
@@ -0,0 +1,43 @@
+import type React from 'react';
+
+import { Button } from '../Button';
+
+import ChevronRightIcon from '../../../assets/svg/icons/chevron-right.svg?react';
+
+import styles from './Section.module.css';
+
+interface SectionProps {
+  title: string;
+  buttonTitle: string;
+  onClick?: () => void;
+  children?: React.ReactNode;
+}
+
+export const Section: React.FC<SectionProps> = ({
+  title,
+  buttonTitle,
+  onClick,
+  children,
+}: SectionProps) => {
+  return (
+    <section className={styles['section-wrapper']}>
+      <div className={styles['title-wrapper']}>
+        <h2>{title}</h2>
+        <Button
+          style={{
+            width: 'fit-content',
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px',
+          }}
+          onClick={onClick}
+          variant="tertiary"
+        >
+          {buttonTitle}
+          <ChevronRightIcon />
+        </Button>
+      </div>
+      <div className={styles['card-wrapper']}>{children}</div>
+    </section>
+  );
+};
