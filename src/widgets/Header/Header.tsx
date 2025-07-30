85f79e39d022883435c2e52ba125c9531f7c3d28
CLoudXVII <sharkov.vs@outlook.com>
2025-07-30 13:24:06 +0300

    feat: Добавлен условный блок с профилем в header

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,25 +1,48 @@
 import { useState, useRef, useCallback, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from '../../shared/ui/Button';
-import { DropdownBase } from '../../shared/ui/DropdownBase';
-import { SkillsMenu } from '../SkillsMenu';
+import { Link, useNavigate } from 'react-router-dom';
+
 import styles from './Header.module.css';
+
 import logoUrl from '../../assets/svg/logo.svg';
 import chevronDownIcon from '../../assets/svg/icons/chevron-down.svg';
-import moonIcon from '../../assets/svg/icons/moon.svg';
-import { reorderArrayByRows } from '../../shared/lib/utils';
-import type { Category } from '../../shared/lib/types';
-import { useSelector } from '../../app/store';
+
+import MoonIcon from '../../assets/svg/icons/moon.svg?react';
+import LikeIcon from '../../assets/svg/icons/like-hollow.svg?react';
+import NotificationIcon from '../../assets/svg/icons/notification.svg?react';
+import LogoutIcon from '../../assets/svg/icons/logout.svg?react';
+
+import { useDispatch, useSelector } from '../../app/store';
 import { selectAllSkills } from '../../features/slices/skillsSlice';
+import {
+  fetchUser,
+  logout,
+  selectAuthUser,
+  selectIsAuth,
+} from '../../features/slices/authSlice';
+
+import { SkillsMenu } from '../SkillsMenu';
+import { Button } from '../../shared/ui/Button';
+import type { Category } from '../../shared/lib/types';
+import { DropdownBase } from '../../shared/ui/DropdownBase';
+import { reorderArrayByRows } from '../../shared/lib/utils';
 
 export const Header = () => {
   const [isSkillsMenuOpen, setIsSkillsMenuOpen] = useState(false);
+  const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const headerRef = useRef<HTMLDivElement>(null);
+  const avatarRef = useRef<HTMLDivElement>(null);
   const skillsButtonRef = useRef<HTMLButtonElement>(null);
   const hoverTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const skillsRaw = useSelector(selectAllSkills);
   const reorderedSkills = reorderArrayByRows<Category>(skillsRaw, 3);
+
+  const isAuth = useSelector(selectIsAuth);
+  const user = useSelector(selectAuthUser);
 
   const handleSkillsMenuOpen = useCallback(() => {
     if (hoverTimeoutRef.current) {
@@ -61,6 +84,15 @@ export const Header = () => {
     handleSkillsMenuClose();
   }, [handleSkillsMenuClose]);
 
+  const handleLogout = () => {
+    dispatch(logout());
+    setIsProfileMenuOpen(false);
+  };
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, [dispatch]);
+
   // Обработка клика вне компонента
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -80,6 +112,25 @@ export const Header = () => {
     }
   }, [isSkillsMenuOpen]);
 
+  // Обработка клика вне компонента
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        avatarRef.current &&
+        !avatarRef.current.contains(event.target as Node)
+      ) {
+        setIsProfileMenuOpen(false);
+      }
+    };
+
+    if (isProfileMenuOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }
+  }, [isProfileMenuOpen]);
+
   // Cleanup timeout при размонтировании
   useEffect(() => {
     return () => {
@@ -92,69 +143,122 @@ export const Header = () => {
   return (
     <header className={styles['header-wrapper']} ref={headerRef}>
       <div className={styles['header-container']}>
-        <Link to="/" className={styles['header-logo']}>
-          <img
-            src={logoUrl}
-            alt="SkillSwap Logo"
-            className={styles['logo-icon']}
-          />
-          <div>SkillSwap</div>
-        </Link>
-
-        <nav className={styles['header-nav']}>
-          <Link to="/about" className={styles['nav-link']}>
-            О проекте
-          </Link>
-          <div
-            onMouseEnter={handleButtonMouseEnter}
-            onMouseLeave={handleButtonMouseLeave}
-          >
-            <Button
-              ref={skillsButtonRef}
-              variant="transparent"
-              onClick={handleSkillsMenuToggle}
-              aria-haspopup="true"
-              aria-expanded={isSkillsMenuOpen}
-            >
-              Все навыки
-              <img
-                src={chevronDownIcon}
-                alt="Стрелка вниз для выпадающего меню"
-                className={styles['chevron-down']}
-              />
-            </Button>
-
-            <DropdownBase
-              isOpen={isSkillsMenuOpen}
-              onClose={handleSkillsMenuClose}
-              triggerRef={skillsButtonRef}
-              onMouseEnter={handleDropdownMouseEnter}
-              onMouseLeave={handleDropdownMouseLeave}
-              className={styles.skillsMenuDropdown}
-            >
-              <SkillsMenu skillsData={reorderedSkills} />
-            </DropdownBase>
-          </div>
-        </nav>
-
-        <div className={styles['header-search']}>
-          <div className={styles['search-container']}>
-            <input
-              type="search"
-              placeholder="Искать навык"
-              className={styles['search-input']}
+        <div className={styles['header-navigation-container']}>
+          <Link to="/" className={styles['header-logo']}>
+            <img
+              src={logoUrl}
+              alt="SkillSwap Logo"
+              className={styles['logo-icon']}
             />
+            <div>SkillSwap</div>
+          </Link>
+
+          <nav className={styles['header-nav']}>
+            <Link to="/about" className={styles['nav-link']}>
+              О проекте
+            </Link>
+            <div
+              onMouseEnter={handleButtonMouseEnter}
+              onMouseLeave={handleButtonMouseLeave}
+            >
+              <Button
+                ref={skillsButtonRef}
+                variant="transparent"
+                onClick={handleSkillsMenuToggle}
+                aria-haspopup="true"
+                aria-expanded={isSkillsMenuOpen}
+              >
+                Все навыки
+                <img
+                  src={chevronDownIcon}
+                  alt="Стрелка вниз для выпадающего меню"
+                  className={styles['chevron-down']}
+                />
+              </Button>
+
+              <DropdownBase
+                isOpen={isSkillsMenuOpen}
+                onClose={handleSkillsMenuClose}
+                triggerRef={skillsButtonRef}
+                onMouseEnter={handleDropdownMouseEnter}
+                onMouseLeave={handleDropdownMouseLeave}
+                className={styles.skillsMenuDropdown}
+              >
+                <SkillsMenu skillsData={reorderedSkills} />
+              </DropdownBase>
+            </div>
+          </nav>
+
+          <div className={styles['header-search']}>
+            <div className={styles['search-container']}>
+              <input
+                type="search"
+                placeholder="Искать навык"
+                className={styles['search-input']}
+              />
+            </div>
           </div>
         </div>
 
-        <div className={styles.moonContainer}>
-          <img src={moonIcon} alt="Иконка луны" className={styles.moonIcon} />
-        </div>
+        {!isAuth ? (
+          <>
+            <button className={styles['icon-container']} onClick={() => {}}>
+              <MoonIcon />
+            </button>
 
-        <div className={styles['header-auth']}>
-          <Button variant="secondary">Войти</Button>
-          <Button variant="primary">Зарегистрироваться</Button>
-        </div>
+            <div className={styles['header-auth']}>
+              <Button variant="secondary">Войти</Button>
+              <Button variant="primary">Зарегистрироваться</Button>
+            </div>
+          </>
+        ) : (
+          <div className={styles['header-profile']}>
+            <button className={styles['icon-container']} onClick={() => {}}>
+              <MoonIcon />
+            </button>
+            <button className={styles['icon-container']} onClick={() => {}}>
+              <NotificationIcon />
+            </button>
+            <button className={styles['icon-container']} onClick={() => {}}>
+              <LikeIcon />
+            </button>
+            <div className={styles['profile']} ref={avatarRef}>
+              <p className={styles['profile__username']}>{user?.name}</p>
+              <div
+                className={styles['avatar-container']}
+                onClick={() => setIsProfileMenuOpen((prev) => !prev)}
+              >
+                <img
+                  className={styles.avatar}
+                  src={user?.avatar}
+                  alt={user?.name}
+                />
+              </div>
+
+              <DropdownBase
+                isOpen={isProfileMenuOpen}
+                onClose={() => setIsProfileMenuOpen(false)}
+                triggerRef={avatarRef}
+                className={styles['profile__dropdown']}
+              >
+                <div className={styles['dropdown__content']}>
+                  <button
+                    onClick={() => navigate('/profile/info')}
+                    className={styles['dropdown__button']}
+                  >
+                    Личный кабинет
+                  </button>
+                  <button
+                    onClick={handleLogout}
+                    className={styles['dropdown__button']}
+                  >
+                    Выйти из аккаунта <LogoutIcon />
+                  </button>
+                </div>
+              </DropdownBase>
+            </div>
+          </div>
+        )}
       </div>
     </header>
   );
