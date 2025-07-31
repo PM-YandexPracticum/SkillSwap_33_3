508d8eb4678012eeddedb75d164e31419ea108e6
CLoudXVII <sharkov.vs@outlook.com>
2025-07-31 12:05:49 +0300

    feat: FormInput принимает svg иконку

--- src/shared/ui/FormInput/FormInput.tsx
+++ src/shared/ui/FormInput/FormInput.tsx
@@ -5,6 +5,7 @@ interface FormInputProps extends InputHTMLAttributes<HTMLInputElement> {
   title: string;
   placeholder: string;
   error?: string;
+  svg?: React.ReactNode;
 }
 
 export const FormInput: FC<FormInputProps> = ({
@@ -12,17 +13,30 @@ export const FormInput: FC<FormInputProps> = ({
   placeholder,
   className,
   error,
+  svg,
   ...props
 }) => {
   return (
     <label className={`${styles.label} ${className || ''}`}>
       <span className={styles.title}>{title}</span>
-      <input
-        type="text"
-        className={`${styles.input} ${error ? styles.error : ''}`}
-        placeholder={placeholder}
-        {...props}
-      />
+      {!svg ? (
+        <input
+          type="text"
+          className={`${styles.input} ${error ? styles.error : ''}`}
+          placeholder={placeholder}
+          {...props}
+        />
+      ) : (
+        <div className={styles['svg-wrapper']}>
+          <input
+            type="text"
+            className={`${styles.input} ${error ? styles.error : ''}`}
+            placeholder={placeholder}
+            {...props}
+          />
+          {svg}
+        </div>
+      )}
       {error && <span className={styles.errorMessage}>{error}</span>}
     </label>
   );
